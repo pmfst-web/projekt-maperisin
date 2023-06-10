ae3a61689fc4218ad5d910548802029ac692697d
Martina Perišin <perisinmartina@gmail.com>
2023-06-10 14:20:19 +0200

    Dodan početni kod za StatistikaEkran

---
+++ screens/StatistikaEkran.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const StatistikaEkran = () => {
+    return (
+      <View style={styles.container}>
+        <Text>Statistika ekran</Text>
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+  });
+  
+  export default StatistikaEkran;
